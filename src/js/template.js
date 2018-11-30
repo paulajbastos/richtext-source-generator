2207535b0f58545debc1aa36e8cb494c5d40f395
paula.junqueira <paula.junqueira@PaulaJunqueira-Mac-mini.local>
2018-11-30 16:24:34 -0200

    changing to swiperjs

--- src/js/template.js
+++ src/js/template.js
@@ -1,19 +1,16 @@
-// import 'slick-carousel';
-// import 'slick-carousel/slick/slick';
-
 export default function template() {
   console.log("template JS loaded");
 
-  $(window).load(function () { // On load
-    fullHeightSlider();
-  });
-  $(window).resize(function () { // On resize
-    fullHeightSlider();
-  });
-  function fullHeightSlider() {
-    $('.faixa-beneficios').attr('id', 'faixaBeneficios');
-    document.getElementById("sliderPrincipal").style.height = ($(window).height() - ($('#headerBar').height() + $('#faixaBeneficios').height()) + 'px');
-  };
+  // $(window).load(function () { // On load
+  //   fullHeightSlider();
+  // });
+  // $(window).resize(function () { // On resize
+  //   fullHeightSlider();
+  // });
+  // function fullHeightSlider() {
+  //   $('.faixa-beneficios').attr('id', 'faixaBeneficios');
+  //   document.getElementById("sliderPrincipal").style.height = ($(window).height() - ($('#headerBar').height() + $('#faixaBeneficios').height()) + 'px');
+  // };
 
   $(document).ready(function () {
     $(".btnComprarScroll").on('click', function (event) {
@@ -27,21 +24,24 @@ export default function template() {
       }
     });
 
-    $(".Modern-Slider").slick({
+    var mySwiper = new Swiper('.swiper-container', {
       autoplay: true,
-      autoplaySpeed:5000,
-      speed:500,
-      dots: false,
-      slidesToShow:1,
-      slidesToScroll:1,
-      pauseOnHover:false,
-      respondTo:'min',
-      cssEase:'linear',
-      prevArrow:'<button class="PrevArrow"></button>',
-      nextArrow:'<button class="NextArrow"></button>',
-      customPaging : function(slider, i) {
-      var thumbnail = $(slider.$slides[i]).data('thumbnail');
-      return '<a href="javascript:void(0)"><img src="'+thumbnail+'"></a>';},
+      loop: true,
+      // loopedSlides: parseInt(widget.carouselTotalItens()),
+      // slidesPerView: parseInt(widget.carouselTotalItens()),
+      // slidesPerGroup: parseInt(widget.carouselTotalItens()),
+      speed: 1000,
+      // spaceBetween: parseInt(widget.carouselSpaceItens()),
+      // navigation: {
+      //   nextEl: widget.carouselHasArrows() ? '#carouselRecomendationInstance-' + i + ' .swiper-button-next.btn-next-' + i : '',
+      //   prevEl: widget.carouselHasArrows() ? '#carouselRecomendationInstance-' + i + ' .swiper-button-prev.btn-prev-' + i : ''
+      // },
+      // pagination: {
+      //   type: widget.carouselHasBullets() ? 'bullets' : '',
+      //   dynamicBullets: widget.carouselHasBullets() ? true : '',
+      //   clickable: widget.carouselHasBullets() ? true : '',
+      //   el: widget.carouselHasBullets() ? '#carouselRecomendationInstance-' + i + ' .swiper-pagination.pagination-' + i : ''
+      // }
     });
   });
 }
