ccb63beac856bb7a6da195d9591937842908403b
paula.junqueira <paula.junqueira@PaulaJunqueira-Mac-mini.local>
2019-02-25 18:28:49 -0300

    init

--- src/js/template.js
+++ src/js/template.js
@@ -1,6 +1,49 @@
 export default function template() {
-  console.log("template JS loaded");
+    console.log("template JS loaded");
 
-  /* Here you can use whatever JS you want :) */
+    // $(window).load(function(){ // On load
+    //   fullHeightSlider();
+    // });
+    // $(window).resize(function(){ // On resize
+    //   fullHeightSlider();
+    // });
+    // function fullHeightSlider() {
+    //   $('.faixa-beneficios').attr('id','faixaBeneficios');
+    //   document.getElementById("sliderPrincipal").style.height = ($(window).height() - ($('#headerBar').height() + $('#faixaBeneficios').height())+'px');
+    // };
+
+    $(document).ready(function(){
+      
+      $(".btnComprarScroll").on('click', function(event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          });
+          return false;
+        }
+      });
+
+      var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        speed: 900,
+        loop: true,
+        simulateTouch: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: false,
+          dynamicBullets: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+    });
 
 }
