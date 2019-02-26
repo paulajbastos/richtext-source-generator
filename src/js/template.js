167470a787e386e570c8c3429d1eb1e679f55432
paula.junqueira <paula.junqueira@PaulaJunqueira-Mac-mini.local>
2019-02-26 19:13:26 -0300

    landing

--- src/js/template.js
+++ src/js/template.js
@@ -1,6 +1,24 @@
 export default function template() {
   console.log("template JS loaded");
 
-  /* Here you can use whatever JS you want :) */
+  jQuery.ajax({
+    type: 'GET',
+    url: 'https://api-prd.reservapto.com.br/occ-physicalstore-subscribe/store',
+    headers: {
+      'Authorization': 'Basic b2NjOjkxcjQjQG5yeDE3I0BydXQwY2MzIw'
+    },
+    success: function (data) {
+      console.log(data);
+    }
+  });
+
+  var select1 = $( '#Estado' ),
+  select2 = $( '#Loja' ),
+    options = select2.find( 'option' );
+
+  select1.on( 'change', function() {
+    select2.html( options.filter( '[UFdata="' + this.value + '"]' ) );
+  } ).trigger( 'change' );
+
 
 }
