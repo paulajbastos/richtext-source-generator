462bf1969c12054ceb322593243ceecead73c4a5
paula.junqueira <paula.junqueira@PaulaJunqueira-Mac-mini.local>
2019-02-27 18:27:22 -0300

    testing mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
+// import Inputmask from "inputmask";
 import template from './template';
 
+$(document).ready(function(){
+  // var tel = document.getElementById('InputTelefone');
+  // var telMask = new Inputmask('(99)99999-9999');
+  // telMask.mask(tel);
+
+  // var cpf = document.getElementById('InputCPF');
+  // var cpfMask = new Inputmask('999.999.999-99');
+  // cpfMask.mask(cpf);
+});
 template();
