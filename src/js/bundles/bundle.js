c232004d573bd0c13665cffb9f2ca6411cfc47c9
paula.junqueira <paula.junqueira@PaulaJunqueira-Mac-mini.local>
2019-02-27 13:36:03 -0300

    testing and changing css

--- src/js/bundles/bundle.js
+++ src/js/bundles/bundle.js
@@ -1,20 +1,36 @@
 
-// var swiper = new Swiper('.swiper-container', {
-//   slidesPerView: 1,
-//   speed: 900,
-//   loop: true,
-//   simulateTouch: true,
-//   autoplay: {
-//     delay: 3000,
-//     disableOnInteraction: false,
-//   },
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: false,
-//     dynamicBullets: true,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+$(document).ready(function () {
+
+  $(".btnComprarScroll").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      });
+      return false;
+    }
+  });
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    speed: 900,
+    loop: true,
+    simulateTouch: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: false,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
+
+
--- src/js/template.js
+++ src/js/template.js
@@ -1,1 +1,37 @@
-console.log('template');
+// export default function template() {
+//   console.log("template JS loaded");
+//   $(document).ready(function () {
+
+//     $(".btnComprarScroll").on('click', function (event) {
+//       if (this.hash !== "") {
+//         event.preventDefault();
+//         var hash = this.hash;
+//         $('html, body').animate({
+//           scrollTop: $(hash).offset().top
+//         });
+//         return false;
+//       }
+//     });
+
+//     var swiper = new Swiper('.swiper-container', {
+//       slidesPerView: 1,
+//       speed: 900,
+//       loop: true,
+//       simulateTouch: true,
+//       autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//       },
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: false,
+//         dynamicBullets: true,
+//       },
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//     });
+//   });
+
+// }
