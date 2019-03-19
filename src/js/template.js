9e1422050a0d54db52cef4719c213d4354b20960
Imac <imac@Imacs-iMac.local>
2019-03-19 16:39:05 -0300

    sccs and others jquery events

--- src/js/template.js
+++ src/js/template.js
@@ -105,6 +105,17 @@ export default function template() {
     	$('.block-modal-video-ofc').fadeToggle();
     });
 
+    $('.navbar-ofc').slideUp();
+
+    $(document).scroll(function() {
+    	var y = $(this).scrollTop();
+    	if (y > $('.sectiion-pima').offset().top) {
+    		$('.navbar-ofc').slideDown('fast');
+    	} else {
+    		$('.navbar-ofc').slideUp('fast');
+    	}
+	});
+
   	$('.section-produtos-ofc').parallax({imageSrc: 'https://imagens.usereserva.com.br/rsv_assets/oficina/outono-inverno-2019/images/foto_modelo.jpg'});
 	$('.zoom-div-ofc').parallax({imageSrc: 'https://imagens.usereserva.com.br/rsv_assets/oficina/outono-inverno-2019/images/bg-hero.jpg'});
 
