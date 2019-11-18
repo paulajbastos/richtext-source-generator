6c8de4bc8492d8e90c6ff17702734ca67a9a52e5
Julio Jacob <julio.jacob@usereserva.com>
2019-11-18 17:02:20 -0300

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,1 @@
-import template from './template';
-template();
+console.log("funciona");
--- src/js/template.js
+++
@@ -1,5 +0,0 @@
-export default function template() {
-  console.log("template JS loaded");
-
-  /* Here you can use whatever JS you want :) */
-}
