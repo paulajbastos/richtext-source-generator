a2a5fee47d76c13169568ef511b49e6f289942e1
Paula Junqueira <paulajunqueira@Mac-mini-de-Paula.local>
2019-11-21 19:33:14 -0300

    teste

--- src/js/main.js
+++ src/js/main.js
@@ -10,22 +10,27 @@
 
 let bt = document.getElementById('btSend');
 
-bt.onclick = function() { 
+bt.onclick = function() {
     console.log('2019-12-20-15-30');
     let date = '2019-12-20-15-30';
     let id = '5dd5dd691d8f655b98ef6d41';
     const url = `/localhost:8080/api/calendar/${id}`;
     console.log('url', url);
 
+    var data = {
+      "availabilityChecks" : [1,1],
+      "available" : true,
+      "appointment" : date
+    };
 
     jQuery.ajax({
-        url: `/localhost:8080/api/calendar/`,
-        type: 'GET',
-        //data: {"available": false},
+        url: 'http://localhost:8080/api/calendar-update',
+        type: 'POST',
+        data: data,
         success: function(result) {
-            //Do something with the result
-            console.log('result');
-        } 
+          //Do something with the result
+          console.log('result');
+        }
     });
 };
 
