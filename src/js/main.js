8175e58f810c7e11ff2737fef0167afaff3e4139
Paula Junqueira <paulajunqueira@Mac-mini-de-Paula.local>
2019-11-25 10:04:55 -0300

    testes

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,25 @@
-// import template from './template';
-// template();
-
-// let app = {
-//   checkCalendarOption: function(date) {
-//     console.log('date', date)
-//   };
-// }
-
-
 let bt = document.getElementById('btSend');
 
 bt.onclick = function() {
-    console.log('2019-12-20-15-30');
-    let date = '2019-12-20-15-30';
-    let id = '5dd5dd691d8f655b98ef6d41';
-    const url = `/localhost:8080/api/calendar/${id}`;
-    console.log('url', url);
 
-    var data = {
-      "availabilityChecks" : [1,1],
-      "available" : true,
-      "appointment" : date
-    };
+  // "data": "{\n    \"appointment\" : \"2019-12-20-15-30\"\n}"
+  // "data": JSON.stringify(data)
+  var data = {
+    "appointment" : "2019-12-20-15-30"
+  };
 
-    jQuery.ajax({
-        url: 'http://localhost:8080/api/calendar-update',
-        type: 'POST',
-        data: data,
-        success: function(result) {
-          //Do something with the result
-          console.log('result');
-        }
-    });
+  var settings = {
+    "url": "http://localhost:8080/api/calendar-update",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+    },
+    "data": JSON.stringify(data, false)
+  }
+
+  jQuery.ajax(settings).success(function (response) {
+    console.log(response);
+  }).error(function (err) {
+    console.log(err);
+  });
 };
-
-// bt.onclick(function () {
-//     console.log('2019-12-20-15-30');
-// })
-
-//  function checkCalendarOption (date) {
-//     console.log('date', date)
-//   };
